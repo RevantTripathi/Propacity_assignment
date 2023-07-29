383152548a73718d0838dedb15e8c804f78db4f0
Revant Tripathi <revanttripathi8@gmail.com>
2023-07-30 00:11:05 +0530

    nav_responsive

--- src/Side_navbar/side_nav.js
+++ src/Side_navbar/side_nav.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import './side_nav.css'; // Import the CSS file for styling
+import './side_nav.css'; 
+import FolderIcon from '@mui/icons-material/Folder';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const SideNavigation = () => {
+
+  const List = [
+    { name: 'Films', icon:<FolderIcon className='nav_icon'/>,icon2:<ArrowForwardIosIcon className='nav_icon2'/> },
+    { name: 'People', icon:<FolderIcon className='nav_icon'/>,icon2:<ArrowForwardIosIcon className='nav_icon2'/>},
+    { name: 'Planets', icon:<FolderIcon className='nav_icon'/>,icon2:<ArrowForwardIosIcon className='nav_icon2'/> },
+    { name: 'Species', icon:<FolderIcon className='nav_icon'/> ,icon2:<ArrowForwardIosIcon className='nav_icon2'/>},
+    { name: 'Starships', icon:<FolderIcon className='nav_icon'/> ,icon2:<ArrowForwardIosIcon className='nav_icon2'/>},
+    { name: 'Vechicles', icon:<FolderIcon className='nav_icon'/> ,icon2:<ArrowForwardIosIcon className='nav_icon2'/>},
+  ];
   return (
     <div className="side-nav">
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Contact</li>
+       
+       {List.map((List, index) => (<div className="list">
+          <li key={index}>
+           {List.icon} 
+          {List.name}
+          {List.icon2}
+          </li></div>))}
+
+       
       </ul>
     </div>
   );
--- src/top_b/top_b.js
+++ src/top_b/top_b.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './top_b.css'; 
+import star from './star.png';
 
 const TopNav = () => {
   return (
     <>
       <div className="tp_nv">
+        <div className="star_logo"><img src={star} alt="My Logo" /></div>
         <div className="search">
           <input
             type="text"
